b40608229017719a5ecdf4caa416b750c65d36a3
Robert Hopkins <rmhopkins4@gmail.com>
2023-07-31 16:13:49 -0400

    Update app.py

--- sound_change_applier/app.py
+++ sound_change_applier/app.py
@@ -1,21 +1,33 @@
 from manager import apply_sound_changes
 
-# user input
+# These allow global substitutions to input and output.
+# Most importantly, this lets you use digraphs in categories, as either variables or as defined values
 rewrite_rules = {
     "!front!": "F",
     "!back!": "B",
     "sh": "ʃ",
 }
 
+# Variables can only be one character long, (unless defined with rewrite rules).
 categories = {
     "!front!": "ie",
     "!back!": "ou",
     "V": "aeiou",
 }
 
-word_list = ["be", "bfe", "bce"]
+#
+word_list = [
+    "be",
+    "bfe",
+    "bce",
+]
 
-sound_changes = ["b->d/_(a)e"]
+# A sound change looks like: target->replacement/environment.
+# The environment must contain an underline _, representing the part that changes.
+# Rules apply in the order they are written.
+sound_changes = [
+    "b->d/_(a)e",
+]
 
 result = apply_sound_changes(word_list,
                              sound_changes,
